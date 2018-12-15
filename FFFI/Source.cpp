a493a9b4ab9550253ff6b7a8cc41777917af34e8
sienori <iilll7d6f@yahoo.co.jp>
2018-12-15 16:14:27 +0900

    2値化する前にブラーをかけて滑らかにする

--- FFFI/Source.cpp
+++ FFFI/Source.cpp
@@ -7,18 +7,18 @@ int main(int, char**)
 {
   cv::VideoCapture cap(0);
   if (!cap.isOpened()) return -1;
-  cv::Mat frame, hsv, div, out;
+  cv::Mat frame, blur, hsv, div, out;
   cv::namedWindow("edges", 1);
   for (;;)
   {
     cap >> frame;;
-    cv::cvtColor(frame, hsv, cv::COLOR_BGR2HSV);
+    cv::blur(frame, blur, cv::Size(10, 10));
+    cv::cvtColor(blur, hsv, cv::COLOR_BGR2HSV);
     cv::inRange(hsv, cv::Scalar(70, 100, 30), cv::Scalar(120, 255, 200), div);
     cv::erode(div, out, cv::Mat(), cv::Point(-1, -1), 1);
     cv::Moments mu = cv::moments(out);
     cv::Point2f mc = cv::Point2f(mu.m10 / mu.m00, mu.m01 / mu.m00);
     cv::circle(out, mc, 4, cv::Scalar(100), 2, 4);
-
     imshow("edges", out);
     if (cv::waitKey(30) >= 0) break;
   }
