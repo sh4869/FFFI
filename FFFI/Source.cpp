30b6e44d123e067b44e0e8c89521b7761a48ded8
sienori <iilll7d6f@yahoo.co.jp>
2018-12-15 16:15:28 +0900

    波形を描画

--- FFFI/Source.cpp
+++ FFFI/Source.cpp
@@ -8,6 +8,11 @@ int main(int, char**)
   cv::VideoCapture cap(0);
   if (!cap.isOpened()) return -1;
   cv::Mat frame, blur, hsv, div, out;
+
+  const int waveLength = 150;
+  cv::Mat wave(cv::Size(waveLength, 1), CV_32F, cv::Scalar::all(0)), blurredWave;
+
+  cv::namedWindow("wave", 1);
   cv::namedWindow("edges", 1);
   for (;;)
   {
@@ -20,6 +25,29 @@ int main(int, char**)
     cv::Point2f mc = cv::Point2f(mu.m10 / mu.m00, mu.m01 / mu.m00);
     cv::circle(out, mc, 4, cv::Scalar(100), 2, 4);
     imshow("edges", out);
+
+    //wave全体をシフト
+    for (int i = 0; i < waveLength - 1; i++) {
+      wave.at<float>(i) = wave.at<float>(i + 1);
+    }
+
+    //最新の値をwave[waveLength-1]に格納 NaNなら1つ前の値
+    const float waveValue = mc.y / frame.rows;
+    wave.at<float>(waveLength - 1) = isnan(waveValue) ? wave.at<float>(waveLength - 2) : waveValue;
+
+    //waveを平滑化
+    cv::blur(wave, blurredWave, cv::Size(3, 1));
+
+    //グラフを描画
+    for (int i = 1; i < waveLength; i++) {
+      const cv::Point pt1 = cv::Point(frame.cols / waveLength * (i - 1), frame.rows * blurredWave.at<float>(i - 1));
+      const cv::Point pt2 = cv::Point(frame.cols / waveLength * i, frame.rows * blurredWave.at<float>(i));
+      const bool isUp = blurredWave.at<float>(i) < blurredWave.at<float>(i - 1);
+      const cv::Scalar color = isUp ? CV_RGB(255, 0, 0) : CV_RGB(0, 0, 255);
+      cv::line(frame, pt1, pt2, color, 1, CV_AA);
+    }
+    imshow("wave", frame);
+
     if (cv::waitKey(30) >= 0) break;
   }
   return 0;
